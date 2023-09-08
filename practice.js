fb286cf242db0deb6553e6dde5fabc7b59d1b4c7
mamunjim <jimmamun2@gmail.com>
2023-09-08 23:34:11 +0600

    another commit

--- practice.js
+++ practice.js
@@ -7,10 +7,4 @@ for(let i = 1; i <= 10; i++)
 
 console.log(prod);
 
-// var number=[1,2,3,4,5,6,7,8,9,10]
-// var multiply=1;
-// for(i=1;i<number.length;i++){
-//     element=number[i];
-//     multiply=multiply*element;
-// }
-// console.log(multiply);
+
